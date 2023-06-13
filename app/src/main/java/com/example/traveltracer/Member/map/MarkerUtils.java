d7599a654ea68b00241651d5511f45e234d6cb75
choseounghyun <108612586+choseounghyun@users.noreply.github.com>
2023-06-14 01:45:34 +0900

    LocationHelper.java 추가 CheckpointManager.java 추가 LocationUtils.java 삭제
    체크포인트 생성 완료

--- app/src/main/java/com/example/traveltracer/Member/map/MarkerUtils.java
+++ app/src/main/java/com/example/traveltracer/Member/map/MarkerUtils.java
@@ -6,10 +6,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Map;
-
 public class MarkerUtils {
-    private locationData locationData;
     private GoogleMap map;
 
     private Marker CheckpointMarker;
@@ -18,44 +15,20 @@ public class MarkerUtils {
         this.map = map;
     }
 
-    public void addMarker(LatLng latLng) {
+   /* public void addMarker(LatLng latLng) {
         map.addMarker(new MarkerOptions().position(latLng));
-    }
-
-    public void updateMarker(LatLng latLng) {
-        // Marker 업데이트 로직 구현
-    }
-
-    public void removeMarker() {
-        map.clear();
-    }
-/*
-    public void madeCheckPoint(Location location){
-        int count =0;
-        //위치 값 비교
-        if(locationData.getnewLongitude() == locationData.getlastLongitude()
-                && locationData.getnewLatitude() == locationData.getlastLatitude() ){
-            count ++;
-            if(count == 1200){
-                map.addMarker(new MarkerOptions().position(latLng));
-            }
-        }
-        else {
-            count = 0;
-        }
+    }*/
 
 
-    }
-    private void createCheckpoint() {
-        // 체크포인트 생성 코드 작성
+    Marker createCheckpoint(LatLng latLng) {
+
         // 위치 정보와 시간 정보를 저장하여 체크포인트 생성
         MarkerOptions markerOptions = new MarkerOptions()
                 .position(latLng)
                 .title("Checkpoint")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-        CheckpointMarker = Map.addMarker(markerOptions);
 
-        return Map.addMarker(markerOptions);
+        return map.addMarker(markerOptions);
     }
-*/
+
 }
