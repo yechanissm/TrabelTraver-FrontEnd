f13654aed3b79e7438ec52922880af3839bd8b9b
lms <ahskdlxm00@naver.com>
2023-06-14 01:58:19 +0900

    xml 파일 수정 및 detail.xml 추가

---
+++ app/src/main/java/com/example/traveltracer/Member/activity/Post_Detail.java
@@ -0,0 +1,16 @@
+package com.example.traveltracer.Member.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.traveltracer.R;
+
+public class Post_Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_detail);
+    }
+}
